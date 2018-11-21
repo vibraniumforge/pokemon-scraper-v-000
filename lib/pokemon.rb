1aba47287475504821f461f6c68f86ad3b7c135d
Greg Morgan <gregm9999@gmail.com>
2018-11-21 20:47:58 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,14 +10,7 @@ class Pokemon
 
   def self.find(id_num, db)
     pokemon_result = db.execute("SELECT * FROM pokemon WHERE id = ?", id_num).flatten
-    # binding.pry
     Pokemon.new(id: pokemon_result[0], name:pokemon_result[1], type:pokemon_result[2], hp: pokemon_result[3], db: db)
-    # new_pokemon = self.new(pokemon)
-    # new_pokemon.id = pokemon[0][0]
-    # new_pokemon.name = pokemon[0][1]
-    # new_pokemon.type = pokemon[0][2]
-    # new_pokemon.hp = pokemon[0][3]
-    # new_pokemon
   end
 
   def alter_hp(num, db)
